c2f6d3e932ccc4453f955b5651f81740c62979e9
l1va <lev_matematik@tut.by>
2018-04-25 00:55:23 +0300

    fix for not active icon

---
+++ background.js
@@ -0,0 +1,18 @@
+	
+'use strict';	
+	
+chrome.runtime.onInstalled.addListener(function() {	
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {	
+    chrome.declarativeContent.onPageChanged.addRules([	
+      {	
+        conditions: [	
+          new chrome.declarativeContent.PageStateMatcher({	
+            pageUrl: { hostPrefix: 'scholar.google' },	
+          })	
+        ],	
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]	
+      }	
+    ]);	
+  });	
+});	
+	
